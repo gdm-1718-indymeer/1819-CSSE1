0c5d9e4a6d6bf2bbbed16c0c681018e467f1e19b
gdm-1718-indymeer <indymeer@student.arteveldehs.be>
2018-10-22 01:18:01 +0200

    alles

---
+++ lesweken/lesweek3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Het afdrukken van een tekts in de console.
+            Console.WriteLine("Hello World!");
+
+  
+
+            int resultaat = (107 * 3)+(5 * deling(optellen(406, 7367), 69)) + vermeningvuldigen(73, 41) - 39 + vermeningvuldigen(7, 8) - 19;
+
+
+            Console.WriteLine("het resultaat is: " + resultaat);
+        }
+
+        static int optellen(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int vermeningvuldigen(int a, int b)
+        {
+            return a * b;
+        }
+        static int deling(int a, int b)
+        {
+            return a/b;
+        }
+
+       static int verhogen(int a)
+        {
+            return a++;
+        }
+       
+    }
+}
